43dc0d1850064ecbde3884811d867acbbe545b08
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-21 01:47:44 +0300

    Removed blog side and added new components

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,45 @@
 import AboutMe from '../components/aboutMe'
+import RecentExperiences from '../components/recentExperiences'
 import FeaturedWorks from '../components/featuredWorks'
 import Layout from '../components/layout'
-import RecentPosts from '../components/recentPosts'
-import Posts from '../models/posts'
+import RecentSkills from '../components/recentSkills'
+import Skills from '../models/skills'
 import Works from '../models/works'
+import Experiences from '../models/experiences'
 
-const HomePage = ({ recentPosts, featuredWorks }) => {
+const HomePage = ({
+  featuredWorks,
+  allSkills,
+  skillsCategories,
+  recentExperiences
+}) => {
   return (
     <Layout>
       <AboutMe />
-      {recentPosts.length > 0 && <RecentPosts recentPosts={recentPosts} />}
+      <RecentExperiences experiences={recentExperiences} />
       <FeaturedWorks featuredWorks={featuredWorks} />
+      <RecentSkills all={allSkills} categories={skillsCategories} />
     </Layout>
   )
 }
 
 export const getStaticProps = async () => {
-  const recentPosts = Posts.recentPosts()
   const featuredWorks = Works.featuredWorks()
-  const response = await Promise.all([recentPosts, featuredWorks])
+  const allSkills = Skills.allSkills()
+  const skillsCategories = Skills.categories()
+  const recentExperiences = Experiences.all()
+  const response = await Promise.all([
+    recentExperiences,
+    featuredWorks,
+    allSkills,
+    skillsCategories
+  ])
   return {
     props: {
-      recentPosts: response[0],
-      featuredWorks: response[1]
+      recentExperiences: response[0],
+      featuredWorks: response[1],
+      allSkills: response[2],
+      skillsCategories: response[3]
     },
     revalidate: 600
   }
