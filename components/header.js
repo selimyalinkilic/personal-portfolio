01528b9cce9f8055bfa5b55f04961cc9d8012a9b
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-21 01:42:08 +0300

    Removed contact link and updated responsive style

--- components/header.js
+++ components/header.js
@@ -3,12 +3,11 @@ import NavLink from './navlink'
 const Header = () => {
   return (
     <header className="flex py-7">
-      <div className="container w-screen mx-auto">
+      <div className="container w-screen px-6 sm:px-0 mx-auto">
         <nav className="flex justify-end">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/works">Works</NavLink>
           <NavLink to="/blog">Blog</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
         </nav>
       </div>
     </header>
