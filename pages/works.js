94f46690fd2f100371d6aa6fd90cbbc2b648b5f5
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-21 01:47:14 +0300

    Updated works page and added new datas

--- pages/works.js
+++ pages/works.js
@@ -1,16 +1,48 @@
 import Layout from '../components/layout'
 import Head from 'next/head'
-const Works = () => {
+import Works from '../models/works'
+import FeaturedWorksCard from '../components/featuredWorksCard'
+const WorksPage = ({ works }) => {
   return (
     <Layout>
       <Head>
         <title> Works | Selim Yalinkilic </title>
       </Head>
-      <div className="container mx-auto text-center h-screen">
-        <h1 className="text-2xl">Welcome to Works Page</h1>
+      <div className="container mx-auto px-6 sm:px-0 text-center h-screen">
+        <div className="flex mt-10">
+          <h1 className="font-bold text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
+            Works
+          </h1>
+        </div>
+        <div className="flex flex-col mt-10">
+          {works &&
+            works.map((work) => (
+              <FeaturedWorksCard
+                key={work.Id}
+                name={work.Name}
+                thumbnail={work.Thumbnail}
+                created={work.Created}
+                summary={work.Summary}
+                category={work.Category}
+                url={work.Url}
+              />
+            ))}
+        </div>
       </div>
     </Layout>
   )
 }
 
-export default Works
+export const getStaticProps = async () => {
+  const all = Works.all()
+
+  const response = await Promise.all([all])
+  return {
+    props: {
+      works: response[0]
+    },
+    revalidate: 600
+  }
+}
+
+export default WorksPage
