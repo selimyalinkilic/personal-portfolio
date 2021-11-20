bde179e31fb96d0a91cd7e09f131e736d1b2d49b
selimyalinkilic <yalinkilicselim@gmail.com>
2021-11-21 01:46:01 +0300

    Updated blog page

--- pages/blog.js
+++ pages/blog.js
@@ -6,8 +6,15 @@ const Blog = () => {
       <Head>
         <title> Blog | Selim Yalinkilic </title>
       </Head>
-      <div className="container mx-auto text-center h-screen">
-        <h1 className="text-2xl">Welcome to Blog Page</h1>
+      <div className="container mx-auto px-6 sm:px-0 text-center h-screen">
+        <div className="flex mt-10">
+          <h1 className="font-bold text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
+            Blog
+          </h1>
+        </div>
+        <div className="flex flex-col mt-40">
+          <p className="text-3xl">Stay tuned, see you soon !</p>
+        </div>
       </div>
     </Layout>
   )
