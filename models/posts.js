935c5171f4c224485d5f01606178768ec25c9667
selimyalinkilic <yalinkilicselim@gmail.com>
2022-02-03 00:58:35 +0300

    Added single post feature

--- models/posts.js
+++ models/posts.js
@@ -23,6 +23,18 @@ class Posts {
 
     return data.filter((o) => !o.Draft)
   }
+  static async getPost(id) {
+    return new Promise((resolve, reject) => {
+      base('Posts').find(id, function (err, record) {
+        if (err) {
+          console.error(err)
+          reject({ err })
+        }
+        const airtablePost = record._rawJson.fields
+        resolve(airtablePost)
+      })
+    })
+  }
 }
 
 export default Posts
