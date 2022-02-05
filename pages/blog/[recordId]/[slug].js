33eebd79aeb6597ac1f6bf27e889d3402d9a2264
selimyalinkilic <yalinkilicselim@gmail.com>
2022-02-06 01:33:42 +0300

    Updated pages style and added meta tags

--- pages/blog/[recordId]/[slug].js
+++ pages/blog/[recordId]/[slug].js
@@ -5,29 +5,79 @@ import Posts from '../../../models/posts'
 import { BsCalendar2Event, BsClock } from 'react-icons/bs'
 import { DateTime } from '../../../utils/datetime'
 import ReactMarkdown from 'react-markdown'
+import { readingTime } from '../../../utils/readingTime'
 
 const BlogDetail = ({ post }) => {
   return (
     <Layout>
       <Head>
         <title>Blog | {post.Title}</title>
+        <meta name="title" content={post.Title} />
+        <meta name="description" content={post.Summary} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`https://selimyalinkilic.com/blog/${post.RecordId}/${post.Slug}`}
+        />
+        <meta property="og:title" content={post.Title} />
+        <meta property="og:description" content={post.Summary} />
+        <meta
+          property="og:image"
+          content={post.Thumbnail[0].thumbnails.large.url}
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content={`https://selimyalinkilic.com/blog/${post.RecordId}/${post.Slug}`}
+        />
+        <meta property="twitter:title" content={post.Title} />
+        <meta property="twitter:description" content={post.Summary} />
+        <meta
+          property="twitter:image"
+          content={post.Thumbnail[0].thumbnails.large.url}
+        ></meta>
       </Head>
-      <div className="container mx-auto px-6 sm:px-0 text-center min-h-screen mt-14">
-        <h1 className="lg:text-4xl xl:text-5xl font-bold">{post.Title}</h1>
-        <div className="flex justify-center mt-4">
+      <div
+        className="mx-auto px-4 sm:px-5 md:px-6 lg:px-0 min-h-screen mt-14"
+        style={{ maxWidth: '850px' }}
+      >
+        <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-center text-dark">
+          {post.Title}
+        </h1>
+        <div className="flex justify-center mt-5">
           <div className="flex items-center">
-            <BsCalendar2Event size="18px" color="black" />
-            <span className="text-xs font-medium ml-2">
+            <BsCalendar2Event size="20px" className="text-gray-600" />
+            <span className="text-sm font-medium ml-2 text-gray-600">
               {DateTime(post.Created)}
             </span>
-            <span className="text-base font-medium mx-4">-</span>
-            <BsClock size="18px" color="black" />
-            <span className="text-xs font-medium ml-2">6 min.</span>
+            <span className="text-sm font-medium mx-4 text-gray-600">-</span>
+            <BsClock size="20px" className="text-gray-600" />
+            <span className="text-sm font-medium ml-2 text-gray-600">
+              {readingTime(post.Content)} min read
+            </span>
           </div>
         </div>
-        <div className="flex flex-col items-start">
-          <ReactMarkdown children={post.Content} />
+        <div className="text-center mt-10">
+          {post.CategoryNames.map((cat, i) => (
+            <span
+              className="text-sm text-primary after:content-['|'] after:px-3 last:after:hidden"
+              key={i}
+            >
+              {cat}
+            </span>
+          ))}
         </div>
+        <figure className="w-100 rounded-2xl overflow-hidden mt-12">
+          <img
+            src={post.Thumbnail[0].url}
+            className="w-100 h-100 block"
+            loading="lazy"
+          />
+        </figure>
+        <ReactMarkdown
+          children={post.Content}
+          className="prose prose-sm mx-auto my-14 w-full max-w-none"
+        />
       </div>
     </Layout>
   )
